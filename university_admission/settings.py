bb98cf05c868b5033cd2497ad92a25814a10d895
simpltm <temurtohirov15@gmail.com>
2025-06-14 01:34:19 +0500

    settings.py: HTTPS va xavfsizlik sozlamalari ozgartirildi

--- university_admission/settings.py
+++ university_admission/settings.py
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
@@ -175,14 +175,15 @@ LOGOUT_REDIRECT_URL = 'applications:application_list'
 # Email settings (for development)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+
 # Security settings
-if not DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    X_FRAME_OPTIONS = 'SAMEORIGIN'
-    SECURE_HSTS_SECONDS = 31536000  # 1 year
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
+#if not DEBUG:
+   # SECURE_SSL_REDIRECT = True
+   # SESSION_COOKIE_SECURE = True
+   #CSRF_COOKIE_SECURE = True
+    #SECURE_BROWSER_XSS_FILTER = True
+    #SECURE_CONTENT_TYPE_NOSNIFF = True
+    #X_FRAME_OPTIONS = 'SAMEORIGIN'
+    #SECURE_HSTS_SECONDS = 31536000  # 1 year
+    #SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    #SECURE_HSTS_PRELOAD = True
