aa488ffd22899155fd5a3c359f9a3f3e32e7ca24
simpltm <temurtohirov15@gmail.com>
2025-06-14 01:40:37 +0500

    settings.py: HTTPS va xavfsizlik sozlamalari ozgartirildi yana

--- university_admission/settings.py
+++ university_admission/settings.py
@@ -176,14 +176,3 @@ LOGOUT_REDIRECT_URL = 'applications:application_list'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-# Security settings
-#if not DEBUG:
-   # SECURE_SSL_REDIRECT = True
-   # SESSION_COOKIE_SECURE = True
-   #CSRF_COOKIE_SECURE = True
-    #SECURE_BROWSER_XSS_FILTER = True
-    #SECURE_CONTENT_TYPE_NOSNIFF = True
-    #X_FRAME_OPTIONS = 'SAMEORIGIN'
-    #SECURE_HSTS_SECONDS = 31536000  # 1 year
-    #SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    #SECURE_HSTS_PRELOAD = True
