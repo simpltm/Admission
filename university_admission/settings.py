d34dfa03a837a870749dbc1a292a8e5faee1f419
simpltm <temurtohirov15@gmail.com>
2025-06-13 17:30:55 +0500

    Update ALLOWED_HOSTS for productionS

--- university_admission/settings.py
+++ university_admission/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-developmen
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['16.170.223.34', 'localhost', '127.0.0.1']
 
 
 # Application definition
