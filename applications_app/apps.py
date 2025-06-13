43c17bc9fd53944e19738821791c0cec9492c17e
simpltm <temurtohirov15@gmail.com>
2025-06-13 13:17:58 +0500

    Move Subject creation to AppConfig.ready()

--- applications_app/apps.py
+++ applications_app/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from django.db import OperationalError
 
 
 class ApplicationsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'applications_app'
+
+    def ready(self):
+        try:
+            from .models import Subject
+            Subject.objects.get_or_create(name='Русский язык')
+        except OperationalError:
+            # Ma'lumotlar bazasi jadvallari yaratilmaganda xatolikni e'tiborsiz qoldirish
+            pass
